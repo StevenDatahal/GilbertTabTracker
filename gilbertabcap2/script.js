27d0e74365ecde2d832f1ce77866dd024a6ff870
StevenDatahal <81633624+StevenDatahal@users.noreply.github.com>
2021-03-30 10:23:13 -0400

    Update script.js

--- gilbertabcap2/script.js
+++ gilbertabcap2/script.js
@@ -1,4 +1,4 @@
-var tabLimit = 8; // set to desired max
+var tabLimit = 8; // Max Tabs
  
 // on new tab
 chrome.tabs.onCreated.addListener(function(tab){
@@ -10,7 +10,7 @@ chrome.tabs.onAttached.addListener(function(tabId, attachInfo){
     checkTabs(tabId);
 });
  
-// on tab replaced, probably not needed, but checking for safety
+// on tab replaced (safety precaution)
 chrome.tabs.onReplaced.addListener(function(addedTabId, removedTabId){
     checkTabs(tabId);
 });
@@ -23,8 +23,8 @@ function checkTabs(tabId = false) {
             if (!tabId) { // no tabId specified, but we're over the limit, so get the id of the last tab in the array
                 tabId = tabs[tabs.length-1].id;
             }
-            chrome.tabs.remove(tabId); // remove the tab
-            alert("You are limited to 8 chrome tabs on this device.");
+            chrome.tabs.remove(tabId); // remove the extra tab
+            alert("You are limited to 8 chrome tabs on this device."); // Alert user (student gets annoyed) 
         }
     });
 }
